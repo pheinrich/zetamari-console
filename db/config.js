8169ad822ddd5df6bd82ad49f6082eb46170b27c
Peter Heinrich <peter.heinrich@gmail.com>
2024-05-14 09:00:33 -0700

    Config for prod database

--- db/config.js
+++ db/config.js
@@ -20,11 +20,7 @@ module.exports =
   },
   production:
   {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
+    use_env_variable: 'PROD_DATABASE_URL',
     dialect: 'mysql',
     dialectModule: mysql2
   }
